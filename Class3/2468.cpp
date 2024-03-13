ec972772bef7d4951ac71306ea6bd49d43367adc
rlarleo <soulen2403@naver.com>
2024-03-13 22:15:05 +0900

    bfs

--- Class3/2468.cpp
+++ Class3/2468.cpp
@@ -5,17 +5,18 @@
 
 using namespace std;
 
-int map[100][100];
-bool visited[100][100];
+int map[500][500];
+bool visited[500][500] = {0, };
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
-int N;
+int N, M;
 
 bool isValid(int x, int y){
-    return (x >= 0 && y >= 0 && x < N && y < N);
+    return (x >= 0 && y >= 0 && x < M && y < N);
 }
 
-void bfs(pair<int, int> start, bool check, int rain){
+int bfs(pair<int, int> start){
+    int result = 1;
     queue<pair<int, int>> que;
     que.push(start);
     visited[start.first][start.second] = 1;
@@ -26,53 +27,44 @@ void bfs(pair<int, int> start, bool check, int rain){
         for(int i = 0; i < 4; i++){
             int nextX = current.second + dx[i];
             int nextY = current.first + dy[i];
-            if(isValid(nextX, nextY) && !visited[nextY][nextX] && (check || map[nextY][nextX] <= rain)){
+            if(isValid(nextX, nextY) && !visited[nextY][nextX] && map[nextY][nextX] == 1){
                 que.push({nextY, nextX});
                 visited[nextY][nextX] = 1;
+                result++;
             }
         }
     }
+    return result;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
     int input;
-    int maxHeight = 0;
-    int maxResult = 1;
-    cin >> N;
+    int result = 0;
+    int count = 0;
+    cin >> N >> M;
+
+    for(int i = 0; i < N; i ++){
+        for(int j = 0; j < M; j++){
+            cin >> input;
+            map[i][j] = input;
+        }
+    }
+
 
     for(int i = 0; i < N; i ++){
         for(int j = 0; j < N; j++){
-            cin >> input;
-            map[i][j] = input;
-            maxHeight = maxHeight < input ? input : maxHeight;
-        }
-    }
-    for(int rain = 0; rain < maxHeight; rain++){
-        memset(visited, 0, sizeof(visited));
-
-
-        for(int i = 0; i < N; i ++){
-            for(int j = 0; j < N; j++){
-                if(!visited[i][j] && map[i][j] <= rain)
-                    bfs({i, j}, false, rain);
+            if(map[i][j] == 1){
+                int curr = bfs({i, j});
+                result = result < curr ? curr : result;
+                count++;
             }
         }
-        
-        int result = 0;
-        for(int i = 0; i < N; i ++){
-            for(int j = 0; j < N; j++){
-                if(!visited[i][j]){
-                    bfs({i, j}, true, rain);
-                    result ++;
-                }
-            }
-        }
-
-        maxResult = maxResult < result ? result : maxResult;
     }
+    
 
-    cout << maxResult;
+
+    cout << count << '\n' << result;
 
     return 0;
 }
