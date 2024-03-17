30af514b6e7d81297c9556076e7787ac960726e6
rlarleo <soulen2403@naver.com>
2024-03-17 21:26:34 +0900

    간단

---
+++ X_Class/31472.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int input;
+    cin >> input;
+    cout << sqrt(input*2) * 4;
+
+    return 0;
+}
