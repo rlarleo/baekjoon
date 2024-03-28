7d9ab296a3d79730cd86fe356193306d59990ae2
rlarleo <soulen2403@naver.com>
2024-03-28 21:19:45 +0900

    이분탐색 두 포인터

---
+++ Class5/2467.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 100000 + 1;
+
+int N;
+long long int arr[MAX];
+long long int resA, resB;
+
+int main() {
+
+	//입력
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	int left = 0;
+	int right = N - 1;
+
+	int resLiquid = abs(arr[left] + arr[right]);
+	resA = arr[left];
+	resB = arr[right];
+
+	while (left < right) {
+		int tmpLiquid = arr[left] + arr[right];
+		if (abs(tmpLiquid) < resLiquid) {
+			resLiquid = abs(tmpLiquid);
+			resA = arr[left];
+			resB = arr[right];
+		}
+
+		if (tmpLiquid < 0) {
+			left++;
+		}
+		else {
+			right—;
+		}
+	}
+
+	cout << resA << " " << resB << "\n";
+}
