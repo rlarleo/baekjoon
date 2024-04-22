0cff20c146df113e2434692f29952a9a799e7bad
rlarleo <soulen2403@naver.com>
2024-04-22 20:30:26 +0900

    간단한 문제

--- Class3/????.cpp
+++ Class3/????.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
 using namespace std;
 
-int ladder[101];
-int visited[101];
-
-void bfs(){
-    queue<pair<int, int>> que;
-    que.push({1, 0});
-    while(!que.empty()){
-        int current = que.front().first;
-        if(visited[current] != 0 && visited[current] <= que.front().second) {
-            que.pop();
-            continue;
-        }
-        visited[current] = que.front().second;
-        que.pop();
-        if(current <= 100){
-            int next;
-            if(ladder[current] != 0){
-                next = ladder[current];
-                que.push({next, visited[current]});
-            }
-            next = current+1;
-            for(int i = 0; i < 6; i++)
-                que.push({next+i, visited[current] + 1});
-        }
-    }
-    for(int i = 1; i < 101; i++){
-        cout << i << " " << visited[i] << endl;
-    }
-}
-
 int main()
 {
-    cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-
-    int N, M;
-    cin >> N >> M;
-    for(int i = 0; i < N + M; i++){
-        int start, end;
-        cin >> start >> end;
-        ladder[start] = end;
+    string map[10][10];
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            string input;
+            cin >> input;
+            map[i][j] = input;
+        }
     }
-
-    bfs();
-
+    int result = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (map[i][0] == map[i][1] &&
+            map[i][0] == map[i][2] &&
+            map[i][0] == map[i][3] &&
+            map[i][0] == map[i][4] &&
+            map[i][0] == map[i][5] &&
+            map[i][0] == map[i][6] &&
+            map[i][0] == map[i][7] &&
+            map[i][0] == map[i][8] &&
+            map[i][0] == map[i][9])
+        {
+            result++;
+        }
+        if (map[0][i] == map[1][i] &&
+            map[0][i] == map[2][i] &&
+            map[0][i] == map[3][i] &&
+            map[0][i] == map[4][i] &&
+            map[0][i] == map[5][i] &&
+            map[0][i] == map[6][i] &&
+            map[0][i] == map[7][i] &&
+            map[0][i] == map[8][i] &&
+            map[0][i] == map[9][i])
+        {
+            result++;
+        }
+    }
+    result = result > 0 ? 1 : 0;
+    cout << result;
     return 0;
 }
