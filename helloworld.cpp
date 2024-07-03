8dafb740fd7946462fedae4440ca81a90f40b5c9
rlarleo <soulen2403@naver.com>
2024-07-03 22:19:28 +0900

    인구이동 bfs 구현

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,85 +1,103 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <queue>
-#include <tuple>
+#include <cmath>
+#include <stdlib.h>
 
 using namespace std;
-int map[501][501] = {0, };
-bool visited[501][501] = {0, };
-bool flag = false;
+
+int N, L, R;
+int graph[50][50];
+int tempGraph[50][50];
+bool visited[50][50] = {0, };
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
-int N, M;
 
-int result = 0;
+void InitVisited(){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            visited[i][j] = false;
+        }
+    }
+}
 
 bool IsValid(int x, int y){
-    return (x >= 0 && y >= 0 && x < 501 && y < 501 && map[y][x] != -1);
+    return (x >= 0 && y >= 0 && x < N && y < N);
 }
 
-void Bfs(){
-    deque<tuple<int, int, int>> deq;
-    deq.push_front({0, 0, 0});
-    visited[0][0] = 1;
+bool IsOpen(int x, int y, int a, int b){
+    int distance = abs(graph[y][x] - graph[b][a]);
+    return (distance >= L && distance <= R);
+}
 
-    while(!deq.empty()){
-        int curr_x = get<1>(deq.front());
-        int curr_y = get<0>(deq.front());
-        int dist = get<2>(deq.front());
-        deq.pop_front();
-        if(curr_x == 500 && curr_y == 500)
-        {
-            flag = true;
-            cout << dist;
-            break;
-        }
+int Bfs(int y, int x){
+    int sum = graph[y][x];
+    vector<pair<int, int>> visitedPoint;
+    visitedPoint.push_back({y, x});
+
+    visited[y][x] = 1;
+    queue<pair<int, int>> que;
+    que.push({y, x});
+    while(!que.empty()){
+        int curr_y = que.front().first;
+        int curr_x = que.front().second;
+        que.pop();
         for(int i = 0; i < 4; i++){
-            int next_x = curr_x + dx[i];
             int next_y = curr_y + dy[i];
-            if(IsValid(next_x, next_y) && visited[next_y][next_x] == 0){
+            int next_x = curr_x + dx[i];
+            if(IsValid(next_x, next_y) && !visited[next_y][next_x] && IsOpen(curr_x, curr_y, next_x, next_y)){
                 visited[next_y][next_x] = 1;
-                if(map[next_y][next_x] == 1){
-                    deq.push_back({next_y, next_x, dist + 1});
-                }
-                else{
-                    deq.push_front({next_y, next_x, dist});
-                }
+                que.push({next_y, next_x});
+                visitedPoint.push_back({next_y, next_x});
+                sum += graph[next_y][next_x];
             }
-
         }
-
     }
+
+    int newInput = sum / visitedPoint.size();
+    for(auto p : visitedPoint){
+        tempGraph[p.first][p.second] = newInput;
+    }
+
+
+    return visitedPoint.size() - 1;
+
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int x1, y1, x2, y2;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin >> N >> L >> R;
 
-    cin >> N;
-    for(int i = 0 ; i < N; i++){
-        cin >> x1 >> y1 >> x2 >> y2;
-        for(int y = min(y1, y2); y <= max(y1, y2); y++){
-            for(int x = min(x1, x2); x <= max(x1, x2); x++){
-                map[y][x] = 1;
-            }
-        }
-    }
-    cin >> M;
-    for(int i = 0; i < M; i++){
-        cin >> x1 >> y1 >> x2 >> y2;
-        for(int y = min(y1, y2); y <= max(y1, y2); y++){
-            for(int x = min(x1, x2); x <= max(x1, x2); x++){
-                map[y][x] = -1;
-            }
+    int input;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> input;
+            graph[i][j] = input;
         }
     }
 
-    Bfs();
+    int day = 0;
+    while(true){
+        int move = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(!visited[i][j]){
+                    move += Bfs(i, j);
+                }
+            }
+        }
 
-    if(!flag) cout << "-1";
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                graph[i][j] = tempGraph[i][j];
+            }
+        }        
+        if(move == 0) break;
+        InitVisited();
+        day ++;
+    }
 
-
+    cout << day;
     
-    return 0;
 }
